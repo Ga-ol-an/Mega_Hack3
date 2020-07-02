d564e1082780178c7a4aa99a179d9580e2dd5b95
Gabriel Andrade <gabrielolandrade@hotmail.com.br>
2020-07-02 11:26:21 -0300

    Base do aplicativo adicionada

---
+++ crate-react-app-pronto/front_end/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+/* import Header from './Components/Header' */
+
+import './global.css'
+
+function App() {
+
+  return (
+
+    <div className='content'>{/* Não apague essa< */}
+
+
+      <div>Insira o HTML aqui</div>
+      <div> Mas deixe as divs pais desse! Não as apague!</div>
+
+    {/* Não apague essa< */}
+    </div>
+    
+  );
+}
+
+export default App;
