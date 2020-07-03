e8c3e89f7af9b7c6da70ab32890907a10572b4e2
Caroline Moura <54120284+CahMoura@users.noreply.github.com>
2020-07-03 19:11:39 -0300

    *-*
    Remoção do Button e inserção do Link e Fi do React

--- crate-react-app-pronto/front_end/src/Pages/Leitura/Header_leitura.js
+++ crate-react-app-pronto/front_end/src/Pages/Leitura/Header_leitura.js
@@ -1,18 +1,19 @@
 import React from 'react'
 
 import './style.css'
-
+import {Link} from 'react-router-dom';
 import Logo from '../../Assets/Logo_img_leitura4.svg';
-
+import {FiArrowLeft} from 'react-icons/fi';
 
 export default function Ler() {
 
     return(
         <div className="content_header">
             <img src={Logo} alt="logogg" className="logo"/>
-            <button className="voltar">Voltar</button>
-
-            
+            <Link className="voltar">
+                <FiArrowLeft/>
+                Voltar
+            </Link>
 
         </div>
 
