2dff41f2be6f2b55dfd8fa5ff422e5ee48051fc7
Gabriel Andrade <gabrielolandrade@hotmail.com.br>
2020-07-03 14:41:25 -0300

    Adicionei a pagina após clicar no livro

--- crate-react-app-pronto/front_end/src/Pages/Livro/index.js
+++ crate-react-app-pronto/front_end/src/Pages/Livro/index.js
@@ -1,10 +1,44 @@
-import React from 'react'
+import React from "react";
 
-export default function Livro (){
+import Header from "./../../Components/Header";
 
+import img1 from "./../../Assets/img_livro (1).jpg";
+
+import "./style.css";
+
+export default function Livro() {
     return (
         <div>
-            testandoooo
+            <Header></Header>
+            <div className="content_pag_livro">
+                <div className="image">
+                    <img src={img1} alt="capa" className="capa_livro" />
+                </div>
+                <div className="descricao">
+                    <h1 className="titulo">Dom Casmurro</h1>
+
+                    <div className="infos">
+                        <div className="autor">Autor : Machado de Assis</div>
+
+                        <div className="editora">Editora: Árvore de Livros</div>
+                    </div>
+
+                    <div className="retangulo_rosa">o retangulo rosa aqui</div>
+
+                    <div className="resumo">
+                        Um dos maiores clássicos da literatura brasileira
+                        apresenta um dos maiores mistérios das obras de Machado
+                        de Assis. O autor não se limita a promover o debate
+                        sobre a traição de Capitu, como o perpetua através das
+                        gerações de leitores de uma... Ver mais
+                    </div>
+                    <div className="botoes">
+                        <button>botao 1</button> 
+                        <button>botao 2</button> 
+                        <button>botao 3</button> 
+                    </div>
+                </div>
+            </div>
         </div>
-    );  
+    );
 }
